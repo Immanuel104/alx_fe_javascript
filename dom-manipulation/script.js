fd56a9dcb399b4e72842ade3b3f526e855539dd7
Immanuel104 <ndundaimmanuel61@gmail.com>
2024-10-19 09:51:15 +0300

    my files

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -16,6 +16,7 @@ function saveQuotes() {
 function addQuote() {
   const newQuoteText = document.getElementById('newQuoteText').value;
   const newQuoteCategory = document.getElementById('newQuoteCategory').value;
+     "addEventListener"
 
   if (newQuoteText && newQuoteCategory) {
     const newQuote = {
